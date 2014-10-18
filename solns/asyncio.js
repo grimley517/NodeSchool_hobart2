856ecc6a313620870e22c3bf6b01d6d9f36078b9
Brian Jones <ba.jones@me.com>
2014-10-18 01:17:45 +0000

    onto task 5

--- solns/asyncio.js
+++ solns/asyncio.js
@@ -1,9 +1,12 @@
+function myCallback(err,data){
+    var string = data.toString();
+    var arrayOfLines = string.split('\n');
+    //console.log(arrayOfLines);
+    console.log(arrayOfLines.length-1);
+}
+
 var fs = require('fs');
 var filePath = process.argv[2]
-var buffer = fs.readFileSync(filePath);
-var string = buffer.toString();
-var arrayOfLines = string.split('\n');
-//console.log(arrayOfLines);
-console.log(arrayOfLines.length-1);
+fs.readFile(filePath,'utf-8',myCallback)
 
 
