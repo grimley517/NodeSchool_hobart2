929633e113cb534b4e0b0fccd11d9575119ff52a
Brian Jones <ba.jones@me.com>
2014-10-18 04:16:39 +0000

    juggling

--- solns/collect.js
+++ solns/collect.js
@@ -1,11 +1,24 @@
 var web = require ('http');
 
 var target = process.argv[2];
+var dataList = [];
 
 web.get(target,function callBk(response){
     response.setEncoding('utf-8');    
     response.on('data',function(data){
-        console.log(data);
+        dataList.push(data);
     });
+    response.on('end', function(){
+        var len = 0;
+        dataList.forEach(function(element){
+            len += element.length;
+        })
+        var words = ''
+        dataList.forEach(function(element){
+            words += element;
+        })
+        console.log(len);
+        console.log(words);
+    })
 })
     
